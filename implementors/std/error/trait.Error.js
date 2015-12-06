01d8e0026bb622edd23bf85a7c73a7724d207a3a
Travis CI <noreply@travis-ci.com>
2015-12-06 11:37:26 +0000

    Update documentation

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['shared_library'] = [];implementors['objc'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='objc/declare/struct.UnequalArgsError.html' title='objc::declare::UnequalArgsError'>UnequalArgsError</a>",];implementors['gleam'] = [];implementors['cocoa'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='objc/declare/struct.UnequalArgsError.html' title='objc::declare::UnequalArgsError'>UnequalArgsError</a>",];implementors['glutin'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='glutin/enum.CreationError.html' title='glutin::CreationError'>CreationError</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='glutin/enum.ContextError.html' title='glutin::ContextError'>ContextError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
